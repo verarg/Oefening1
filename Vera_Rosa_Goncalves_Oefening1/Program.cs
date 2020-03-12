70be81974ab25d93b57fcc49d3fd2be8e5e7c62e
vera rosa g <vera.rosa.goncalves@telenet.be>
2020-03-12 15:39:33 +0100

    Add project files.

---
+++ Vera_Rosa_Goncalves_Oefening1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vera_Rosa_Goncalves_Oefening1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
